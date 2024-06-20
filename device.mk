0b13371d97f121015e772e30d80bbd998712ef79
Giovanni Ricca <giovanniricca@proton.me>
2024-06-20 13:10:11 +0530

    selene: Switch to SW C2
    * Bye SW OMX
    
    Change-Id: I3518b0d68c7aaef2376162078b98833e0abeff77
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: SHADOW <33841315+SHADOW-REX@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -256,6 +256,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/media_codecs_mediatek_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_mediatek_audio.xml \
     $(DEVICE_PATH)/configs/media/media_codecs_mediatek_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_mediatek_video.xml \
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
+
 # Media Codec2 modules
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
