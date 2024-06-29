a1bf87368420a3f900293423cb6ac9636e0543a2
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2024-06-30 01:22:48 +0530

    selene: patch libMtkOmxVdecEx.so
    also update sha1sum
    *needed after A14 QPR3
    
    E osal_utils: dlopen failed, dlopen failed: cannot locate symbol _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_ referenced by /vendor/lib/libMtkOmxVdecEx.so
    
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -80,6 +80,9 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
+        vendor/lib/libMtkOmxVdecEx.so)
+            grep -q "libui_shim.so" "$2" || "$PATCHELF" --add-needed "libui_shim.so" "$2"
+            ;;
     esac
 }
 
