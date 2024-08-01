b904b0f5fd4d445716a5f745568995cc99c00358
hasan6034 <hasanbasrioy@gmail.com>
2024-08-01 10:07:42 +0300

    selene: Inculude device FCM for stock HALs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -221,6 +221,9 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml
+
 ODM_MANIFEST_SKUS += \
     eos
 
