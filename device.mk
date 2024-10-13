a28c7fbf4eb6eab1829e8ba18cf89529eab638f7
johnmart19 <johnivan19999@gmail.com>
2024-10-13 19:25:52 +0530

    selene: Build `android.hardware.media.omx` from source
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -248,6 +248,9 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-service
 
 # Media
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(DEVICE_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
